fbd6b25d8bbda9ee1f1ac391bbbdac5c66a9dd2d
QuyenNguyen <quyen.nguyen2@ntq-solution.com.vn>
2018-02-21 18:04:40 +0700

    angular_core_http

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+
+export class StudentService {
+  private apiUrl = "https://5a8d3d92d6c8840012dde9c5.mockapi.io/Angular/Student";
+
+  constructor(private _http: Http) {}
+
+  GetList(): Observable<any[]> {
+    return this._http.get(this.apiUrl).map((response: Response) => response.json());
+  }
+}
